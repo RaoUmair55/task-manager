72544e72c8885873e58ce0ebbbb7422658a0910d
RaoUmair55 <raoumair554@gmail.com>
2025-01-07 23:11:38 +0500

    fix add task

--- obj/Debug/net6.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net6.0/TaskManager.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ad73d2ddd508f3f7d67df9ea1cbf74c1a98008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2d71b997a5bb4ac4547c8e1a5050d11a573c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
